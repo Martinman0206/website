d1b8da35077455436bc824002331fb57f4c4f24e
Martinman0206 <martinman0206@gmail.com>
2025-07-30 15:01:49 -0400

    Add files via upload

---
+++ about-me-site/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AboutMeSite from './AboutMeSite';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AboutMeSite />
+  </React.StrictMode>
+);
